488c98f6fe5c4c512e33b7f163809eef89b31013
habibapanna <habibapanna49@gmail.com>
2026-01-16 11:45:03 +0600

    updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     >
 
       {/* Content */}
-      <div className="relative z-10 mx-auto ml-20 md:ml-[192px] mt-60 md:mt-[250px] md:mb-[276px] grid grid-cols-1 lg:grid-cols-2 items-center">
+      <div className="relative z-10 mx-auto ml-20 md:ml-[150px] mt-60 md:mt-[250px] md:mb-[276px] grid grid-cols-1 lg:grid-cols-2 items-center">
         
         {/* LEFT CONTENT */}
         <div className="text-white">
