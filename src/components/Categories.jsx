55c48d2743a694bdf257d4d7fb8cd1f1c51d0638
habibapanna <habibapanna49@gmail.com>
2026-01-28 05:23:42 +0600

    added scroll bar

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,52 +3,52 @@ const items = [
   {
     title: "All Perfumes",
     product: "https://i.postimg.cc/x1BhnzNr/c082e350-40e9-4486-acfd-e19a5713042c-1-(2).png",
+    key: "relevance"
   },
   {
     title: "New Arrivals",
     product: "https://i.postimg.cc/WpSytKqM/c082e350-40e9-4486-acfd-e19a5713042c-1-(1).png",
+    key: "new"
   },
   {
     title: "Best Sellers",
     product: "https://i.postimg.cc/x1BhnzNr/c082e350-40e9-4486-acfd-e19a5713042c-1-(2).png",
+    key: "best-selling"
   },
   {
     title: "Combo Sales",
     product: "https://i.postimg.cc/x1BhnzNr/c082e350-40e9-4486-acfd-e19a5713042c-1-(2).png",
+    key: "combo-sales" 
   },
   {
     title: "Holiday Sale",
     product: "https://i.postimg.cc/x1BhnzNr/c082e350-40e9-4486-acfd-e19a5713042c-1-(2).png",
+     key: "holiday-sale"
   },
 ];
-const Categories = ({ setSortBy }) => {
+const Categories = ({ sortBy, setSortBy }) => {
     return (
         <div>
-              {/* categories */}
-           <div className=" flex justify-center items-center gap-[16px] py-[32px] overflow-x-scroll 2xl:overflow-x-hidden">
-  {items.map((item, i) => (
-    <div
-      key={i}
-      className="bg-[#F6F7F2] rounded-[24px] flex flex-col 2xl:flex-row items-center p-[8px] 2xl:gap-[16px] 2xl:p-[24px] w-full"
-      onClick={() => {
-              // Map category to sortBy value
-              let sortKey = item.title.toLowerCase().replace(/\s/g, "-");
-              if (item.title === "All Perfumes") sortKey = "relevance"; // show all
-              setSortBy(sortKey);
-            }}
-    >
-      <h3 className="text-[20px] 2xl:text-[35px] font-semibold text-[#1D0B01]">
-        {item.title}
-      </h3>
+<div className="flex justify-center items-center gap-4 py-8 overflow-x-scroll 2xl:overflow-x-hidden ">
+        {items.map((item, i) => {
+          const isActive = sortBy === item.key;
 
-      <img
-        src={item.product}
-        alt={item.title}
-        className="object-cover h-[80px] w-[80px] 2xl:h-[108px] 2xl:w-[108px]"
-      />
-    </div>
-  ))}
-</div>
+          return (
+            <div
+              key={i}
+              onClick={() => setSortBy(item.key)}
+              className='flex flex-col 2xl:flex-row items-center p-2 2xl:p-6 gap-2 2xl:gap-4 text-center cursor-pointer rounded-[24px] bg-[#F6F7F2]
+              '
+            >
+             
+               <h3 className="text-lg 2xl:text-[35px] font-semibold text-left">
+                {item.title}
+              </h3>
+              <img className="h-[108px] w-[108px]" src={item.product} alt="" />
+            </div>
+          );
+        })}
+      </div>
         </div>
     );
 };
