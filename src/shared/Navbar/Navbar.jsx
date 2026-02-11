a9d3495f07f10e04bc19283949a4a609f80425a5
habibapanna <habibapanna49@gmail.com>
2026-02-11 16:28:42 +0600

    updated again nav

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -131,9 +131,9 @@ const navPaddingClass = (() => {
 
 // Determine navbar background (unchanged)
 const navBgClass = isProductList || isContact || isFaq || isAbout || isTerms || isBlogPage || isBlogDetails || isProductDetails || isHolidayOffer
-  ? "bg-[#A0174A] backdrop-blur-2xl rounded-full md:mt-[2px]  lg:mt-[12px]"
+  ? "bg-[#997E3D]/80 backdrop-blur-2xl rounded-full md:mt-[2px]  lg:mt-[12px]"
   : scrolled
-  ? "bg-[#A0174A] backdrop-blur-2xl rounded-full mt-[6px] 2xl:mt-[8px] mx-[16px] 2xl:mx-[32px]"
+  ? "bg-[#997E3D]/80 backdrop-blur-2xl rounded-full mt-[6px] 2xl:mt-[8px] mx-[16px] 2xl:mx-[32px]"
   : "bg-transparent";
 
 // Determine top position
@@ -348,7 +348,7 @@ const navTopClass = isHome ? "top-0" : "top-[44px]";
 <Link to='/about'><button className="cursor-pointer text-[10px] lg:text-[12px] 2xl:text-[15px] uppercase">About Us</button></Link>
 
 
-             <Link to='holidayOffer'> <span className="bg-[#BA9948] text-white gap-[4px] 2xl:gap-[5px] text-[10px] lg:text-[12px] 2xl:text-[15px] px-[14px] lg:px-[20px] py-[8px] rounded-[100px] font-semibold flex items-center justify-center w-[110px] lg:w-[150px] h-[20px] lg:h-[30px] 2xl:w-[170px] 2xl:h-[35px] uppercase">
+             <Link to='holidayOffer'> <span className="bg-[#A0174A] text-white gap-[4px] 2xl:gap-[5px] text-[10px] lg:text-[12px] 2xl:text-[15px] px-[14px] lg:px-[20px] py-[8px] rounded-[100px] font-semibold flex items-center justify-center w-[110px] lg:w-[150px] h-[20px] lg:h-[30px] 2xl:w-[170px] 2xl:h-[35px] uppercase">
                 <img className="h-[10px] w-[10px] lg:h-[16px] lg:w-[16px]" src={Solar} alt="" />
                 Holiday Sale
               </span></Link>
