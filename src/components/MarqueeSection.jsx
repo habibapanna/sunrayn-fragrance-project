72d85b7347e0b6d3bee5880d18c662589cf021ab
habibapanna <habibapanna49@gmail.com>
2026-02-26 18:47:58 +0600

    updated marquee

--- src/components/MarqueeSection.jsx
+++ src/components/MarqueeSection.jsx
@@ -1,24 +1,56 @@
 import Marquee from "react-fast-marquee";
 import { products } from "../data/productsData";
 
+const scentBackgrounds = {
+  Flowery:
+    "https://cdn.shopify.com/s/files/1/0047/4067/7699/files/FLORAL_BUTTON_RIGHT_BG.png?v=1734583632",
+  Warm:
+    "https://cdn.shopify.com/s/files/1/0047/4067/7699/files/WARM_EXPANDED_BOTTOM_RIGHT.png?v=1734522567",
+  Gourmand:
+    "https://cdn.shopify.com/s/files/1/0047/4067/7699/files/EDIBLE_EXPANDED_RIGHT.png?v=1734521715",
+  Fresh:
+    "https://cdn.shopify.com/s/files/1/0047/4067/7699/files/FRESH_BUTTON_RIGHT.png?v=1734358115",
+  Earthy:
+    "https://cdn.shopify.com/s/files/1/0047/4067/7699/files/EARTHY_EXPANDED_RIGHT.png?v=1734522344",
+  Herbal:
+    "https://cdn.shopify.com/s/files/1/0047/4067/7699/files/HERBAL_EXPANDED_RIGHT.png?v=1734522155",
+};
 
 const MarqueeSection = () => {
   return (
-    <section className="">
+    <section className="py-6">
       <Marquee speed={40} gradient={false} pauseOnHover>
-        {products.map((item, index) => (
-          <button
-            key={index}
-            className="bg-[#F5F1EA] text-[#A0174A] py-2 px-6 2xl:px-8 md:py-3 rounded-full
-                       flex items-center text-[14px] lg:text-[20px] font-medium
-                       mx-2 md:mx-4 whitespace-nowrap"
-          >
-            Scent Family
-            <span className="underline font-bold ml-3 cursor-pointer">
-              {item.scentFamily}
-            </span>
-          </button>
-        ))}
+        {products.map((item, index) => {
+          const bgImage = scentBackgrounds[item.scentFamily];
+
+          return (
+            <button
+              key={index}
+              className="relative h-[45px] md:h-[55px] rounded-full overflow-hidden 
+                         flex items-center justify-center
+                         mx-2 md:mx-4 px-8 whitespace-nowrap
+                         text-white font-medium
+                         transition duration-300 hover:scale-[1.05]"
+              style={{
+                backgroundImage: `url(${bgImage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              {/* Dark Overlay */}
+              <div className="absolute inset-0 bg-black/50" />
+
+              {/* Text */}
+              <span className="relative z-10 text-[14px] lg:text-[18px]">
+                Scent Family&nbsp;
+                <span className="underline font-semibold ml-1">
+                  {item.scentFamily}
+                </span>
+              </span>
+            </button>
+          );
+        })}
       </Marquee>
     </section>
   );
